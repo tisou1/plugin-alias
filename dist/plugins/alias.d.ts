f19043e9558c040dc0143bb7e3f072d2ff0fe318
tisou1 <tisou1@qq.com>
2023-02-20 22:49:21 +0800

    chore: 配置

--- dist/plugins/alias.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Plugin } from 'rollup';
-declare function alias(options: any): Plugin;
-export { alias };
--- dist/utils/add.d.ts
+++
@@ -1,2 +0,0 @@
-declare function add<T extends number>(a: T, b: T): number;
-export { add };
